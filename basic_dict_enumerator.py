500ff50ccaaadf2aeb451c71f387020d3882656c
kyliehuch <kyliehuch@umail.ucsb.edu>
2021-05-26 15:32:28 -0700

    created Trinity files for dict ds synth

--- basic_dict_enumerator.py
+++ basic_dict_enumerator.py
@@ -17,25 +17,12 @@ class StructInterpreter(PostOrderInterpreter):
     def eval_concat(self, node, args):
         return args[0] + args[1]
 
-    # Include substring method?
-    """def eval_substr(self, node, args):
-        arg_str = args[0]
-        arg_pos0 = args[1]
-        arg_pos1 = args[2]
-        return arg_str[arg_pos0:arg_pos1]"""
-
     # return name, struct tuples for container data types
     def eval_dict(self, node, args):
         d_name = args[0]
         d_key = args[1]
         d_val = args[2]
         return d_name, {d_key:d_val}
-
-    # include update? --> is this needed?
-    """def eval_dict_update(self, node, args):
-        dict1 = args[0]
-        dict2 = args[1]
-        return dict1.update(dict2)"""
 
     def eval_list(self, node, args):
         l_name = args[0]
